182f0091e77f12c2b0a31a2261461f9a2b4676ba
Mario Visic <mario@mariovisic.com>
2012-12-18 14:40:09 +1100

    Fix issue in schema dumping where ':index_opclass => []' would be added causing migrations issues.

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -13,8 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20120501163758) do
 
-  add_extension "hstore"
-
   create_table "people", :force => true do |t|
     t.inet     "ip"
     t.cidr     "subnet"
